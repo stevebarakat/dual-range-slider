c2989cd7c5c94cb288659d9d4ea874549b0ad0af
Steve <stevebarakat@gmail.com>
2021-06-01 22:37:33 -0400

    check in

--- src/DualRangeSlider.js
+++ src/DualRangeSlider.js
@@ -4,6 +4,8 @@ import "./multi-slider.css";
 const DualRangeSlider = () => {
   const [lowerVal, setLowerVal] = useState(null);
   const [upperVal, setUpperVal] = useState(null);
+  const [upperValX, setUpperValX] = useState(null);
+  const [lowerValX, setLowerValX] = useState(null);
   const lowerValEl = useRef(null);
   const upperValEl = useRef(null);
 
@@ -19,8 +21,9 @@ const DualRangeSlider = () => {
   );
   const newPosition2 = 10 - newValue2 * 0.2;
 
+
   //If the upper value slider is LESS THAN the lower value slider plus one.
-  if (upperVal < lowerVal + 1) {
+  if (upperVal > lowerVal + 1) {
     //The lower slider value is set to equal the upper value slider minus one.
     upperVal && setLowerVal(parseFloat(upperVal) - 1);
     //If the lower value slider equals its set minimum.
@@ -30,17 +33,18 @@ const DualRangeSlider = () => {
     }
   }
   //If the lower value slider is GREATER THAN the upper value slider minus one.
-  if (lowerVal > upperVal - 1 && upperVal < 10) {
+  if (lowerVal < upperVal - 1 && upperVal > 10) {
     //The upper slider value is set to equal the lower value slider plus one.
     lowerVal && setUpperVal(parseFloat(lowerVal) + 1);
 
     //If the upper value slider equals its set maximum.
-    if (upperVal === lowerValEl.current?.max && lowerVal < 0) {
+    if (upperVal === lowerValEl.current?.max && lowerVal > 0) {
       //Set the lower slider value to equal the upper value slider's maximum value minus one.
       setLowerVal(parseFloat(lowerValEl.current?.max) - 1);
     }
 
   }
+
 
   return (
     <div class="multi-range-container">
@@ -61,19 +65,25 @@ const DualRangeSlider = () => {
           value={lowerVal}
           step="0.1"
           id="lower"
+          onMouseMove={e => setLowerValX(e.clientX)}
           onInput={e => setLowerVal(parseFloat(e.target.value))}
         />
         <span
           id="range-color"
           class="range-color"
-          style={{ marginLeft: (lowerVal * 10) + '%', width: (upperVal * 10) - (lowerVal * 10) + '%' }}
+          style={{
+            background: "#000000",
+            // background: "-moz-linear-gradient(left,  #000000 25%, #ffffff 25%, #ffffff 86%, #000000 86%, #000000 86%)",
+            background: `-webkit-linear-gradient(left,  #000000 ${upperValX - 10}px,#ffffff ${upperValX}px,#ffffff ${lowerValX - 10}px,#000000 ${lowerValX}px,#000000 86%)`
+            // background: "linear-gradient(to right,  #000000 25%,#ffffff 25%,#ffffff 86%,#000000 86%,#000000 86%)" 
+          }}
         ></span>
         {upperVal && (
           <output
             style={{ left: `calc(${newValue2}% + (${newPosition2}px))` }}
             className="range-value"
           >
-            <span>{upperVal}</span>
+            <span>up:{upperVal}</span>
           </output>
         )}
         <input
@@ -84,6 +94,7 @@ const DualRangeSlider = () => {
           value={upperVal}
           step="0.1"
           id="upper"
+          onMouseMove={e => setUpperValX(e.clientX - 10)}
           onInput={e => setUpperVal(parseFloat(e.target.value))}
         />
       </div>
